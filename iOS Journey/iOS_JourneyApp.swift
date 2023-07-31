f3dc152e0418f92bd3ef99952888a9db71f53496
NazarStf <hworker98@gmail.com>
2023-07-31 09:21:50 +0300

    Initial Commit

---
+++ iOS Journey/iOS_JourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_JourneyApp.swift
+//  iOS Journey
+//
+//  Created by NazarStf on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_JourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
